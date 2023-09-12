145d2015765dfdf6e00c5895952c449f0fdd5c47
西山　翔 <143977837+knnsg@users.noreply.github.com>
2023-09-12 11:14:34 +0900

    osoi

---
+++ Etc/Laravel/web.php
@@ -0,0 +1,22 @@
+Route::get('/app_user/{id}', [AppUserController::class, 'app_user'])->name('al.app_user’);
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AppUserController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/app_user/{id}', [AppUserController::class, 'app_user'])->name('al.app_user');
+
+Route::get('/', function () {
+    return view('welcome');
+});
